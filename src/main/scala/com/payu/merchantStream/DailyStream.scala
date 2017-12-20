e2fd68a3942282848370c04779d712154e7687d7
Hemant Singh <Hemant.singh@payu.in>
2017-12-20 12:09:37 +0530

    kafka streaming with rolling over window; current issue := ConsumerRecords object not serializable

--- src/main/scala/com/payu/merchantStream/DailyStream.scala
+++ src/main/scala/com/payu/merchantStream/DailyStream.scala
@@ -2,7 +2,7 @@ package com.payu.merchantStream
 
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, TaskContext}
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
@@ -12,12 +12,13 @@ object DailyStream {
 
   def main(args: Array[String]): Unit = {
     if (args.length < 2) {
-      System.err.println(s"""
-                            |Usage: DailyStream <brokers> <topics>
-                            |  <brokers> is a list of one or more Kafka brokers
-                            |  <topics> is a list of one or more kafka topics to consume from
-                            |  <ssl> true if using SSL, false otherwise.
-                            |
+      System.err.println(
+        s"""
+           |Usage: DailyStream <brokers> <topics>
+           |  <brokers> is a list of one or more Kafka brokers
+           |  <topics> is a list of one or more kafka topics to consume from
+           |  <ssl> true if using SSL, false otherwise.
+           |
         """.stripMargin)
       System.exit(1)
     }
@@ -25,49 +26,66 @@ object DailyStream {
     val Array(brokers, topics) = args
 
     // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-//    val isUsingSsl = ssl.toBoolean
+    val sparkConf = new SparkConf().setAppName("merchantAmountStream")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
 
     // Create direct kafka stream with brokers and topics
     val topicsSet = topics.split(",").toSet
     val commonParams = Map[String, Object](
       "bootstrap.servers" -> brokers,
-//      "security.protocol" -> (if (isUsingSsl) "SASL_SSL" else "SASL_PLAINTEXT"),
       "sasl.kerberos.service.name" -> "kafka",
       "auto.offset.reset" -> "earliest",
       "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
       "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
       "group.id" -> "default",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-//      ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG -> "6000",
-
+      "enable.auto.commit" -> (false: java.lang.Boolean),
+      "key.serializer" -> "org.apache.kafka.common.serialization.StringSerializer",
+      "value.serializer" -> "org.apache.kafka.common.serialization.StringSerializer"
     )
 
 
-//    val additionalSslParams = if (isUsingSsl) {
-//      Map(
-//        "ssl.truststore.location" -> "/etc/cdep-ssl-conf/CA_STANDARD/truststore.jks",
-//        "ssl.truststore.password" -> "cloudera"
-//      )
-//    } else {
-//      Map.empty
-//    }
+    val kafkaParams = commonParams
 
-   val kafkaParams = commonParams  //++ additionalSslParams
-
-    val messages: InputDStream[ConsumerRecord[String, String]] =
+    val kafkaStream: InputDStream[ConsumerRecord[String, String]] =
       KafkaUtils.createDirectStream[String, String](
         ssc,
         LocationStrategies.PreferConsistent,
         ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams)
       )
 
+    // reference to the most recently generated input rdd's offset ranges
+    var offsetRanges = Array[OffsetRange]()
+
+
+    kafkaStream.map(rdd => (rdd.value().toString)).print()
+
+    kafkaStream.transform { rdd =>
+
+      // It's possible to get each input rdd's offset ranges, BUT...
+      offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      println("got offset ranges on the driver:\n" + offsetRanges.mkString("\n"))
+      println(s"number of kafka partitions before windowing: ${offsetRanges.size}")
+      println(s"number of spark partitions before windowing: ${rdd.partitions.size}")
+      rdd
+    }.window(Seconds(15), Seconds(2)).foreachRDD { rdd =>
+      //... if you then window, you're going to have partitions from multiple input rdds, not just the most recent one
+      println(s"number of spark partitions after windowing: ${rdd.partitions.size}")
+      rdd.foreachPartition { iter =>
+        println("read offset ranges on the executor\n" + offsetRanges.mkString("\n"))
+        // notice this partition ID can be higher than the number of partitions in a single input rdd
+        println(s"this partition id ${TaskContext.get.partitionId}")
+        iter.take(10).foreach(println)
+      }
+   }
+
+
+
     // Get the lines, split them into words, count the words and print
-    val lines = messages.map(_.value())
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
+    //    val lines = kafkaStream.map(_.value())
+    //    val words = lines.flatMap(_.split(" "))
+    //    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+    //    wordCounts.print()
 
     // Start the computation
     ssc.start()
